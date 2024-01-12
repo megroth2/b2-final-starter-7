3242a7371edc2241bbbb6ff650bdd3d23f24bca7
Meg Roth <146018731+megroth2@users.noreply.github.com>
2024-01-12 10:11:46 -0700

    rename coupon category to amount off

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_12_020234) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_12_164926) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "coupons", force: :cascade do |t|
     t.string "name"
     t.string "code"
-    t.string "category"
+    t.string "amount_off"
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
